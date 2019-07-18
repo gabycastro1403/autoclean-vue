12699e3cf3507b5b9e6eb1dc04fcfc823c9e0b9f
Pau-za <molo_pau@hotmail.com>
2019-07-18 17:18:13 -0500

    changes firebase configuration from main.js to firebase.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import firebase from 'firebase'
+// import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
@@ -15,15 +15,40 @@ new Vue({
   }
 })
 
-const config = {
-  apiKey: "AIzaSyCtnnkI5WnROzwdlNfTpx3GZKgLfuFrnkg",
-  authDomain: "autoclean-vue.firebaseapp.com",
-  databaseURL: "https://autoclean-vue.firebaseio.com",
-  projectId: "autoclean-vue",
-  storageBucket: "autoclean-vue.appspot.com",
-  messagingSenderId: "1062335017347",
-  appId: "1:1062335017347:web:0934c0ec40a82ac4"
-};
+// const config = {
+//   apiKey: "AIzaSyCtnnkI5WnROzwdlNfTpx3GZKgLfuFrnkg",
+//   authDomain: "autoclean-vue.firebaseapp.com",
+//   databaseURL: "https://autoclean-vue.firebaseio.com",
+//   projectId: "autoclean-vue",
+//   storageBucket: "autoclean-vue.appspot.com",
+//   messagingSenderId: "1062335017347",
+//   appId: "1:1062335017347:web:0934c0ec40a82ac4"
+// };
 
-firebase.initializeApp(config);
-export const db = firebase.firestore();
+// firebase.initializeApp(config);
+
+// // login with google
+// const provider = new firebase.auth.GoogleAuthProvider();
+
+// const googleAuth = firebase.auth().signInWithPopup(provider).then(function(result) {
+//   // This gives you a Google Access Token. You can use it to access the Google API.
+//   var token = result.credential.accessToken;
+//   console.log(token)
+//   // The signed-in user info.
+//   var user = result.user;
+//   console.log(user);
+//   // ...
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // The email of the user's account used.
+//   var email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   var credential = error.credential;
+//   // ...
+// });
+
+// const db = firebase.firestore();
+
+// export {db, googleAuth}
