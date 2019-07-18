89754d78d8d1963d9105d8812c6dc1ffe9d56f38
gabycastro1403 <gaby09choco@gmail.com>
2019-07-18 12:43:43 -0500

    Se realiza el componente del form y se agrega el CDN de boostrap

---
+++ src/components/Form.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="form">
+ <form>
+    <div class="form-group">
+        <label for="formGroupExampleInput">Example label</label>
+        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+    </div>
+    <div class="form-group">
+        <label for="formGroupExampleInput2">Another label</label>
+        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+    </div>
+</form>
+        
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'form',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js PWA'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
