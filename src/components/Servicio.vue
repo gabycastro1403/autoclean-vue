e9efb5aef8b85293ba5dc4c51cf13018140aef97
AcheZeta <ache.mariana@gmail.com>
2019-07-18 22:31:43 -0500

    ADD: Style and redirect in Navbar, CREATE: Servicio component

---
+++ src/components/Servicio.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container" id="Servicio">
+    <h1>Servicio</h1>
+    <div class="card border-primary mb-3">
+      <div class="card-header">Juan está lavando tu auto</div>
+      <div class="card-body text-primary">
+        <h5 class="card-title">Estará listo en aprox: 00:14:50</h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+import db from "../js/firebase";
+
+export default {
+  // name: "historial"
+  data() {
+    return {
+      historial: []
+    };
+  }
+};
+</script>
+
+<style>
+</style>
