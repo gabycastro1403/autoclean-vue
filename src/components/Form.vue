7f8199f2b9ec372f26e38608408fad937f7a12aa
gabycastro1403 <gaby09choco@gmail.com>
2019-07-18 23:05:26 -0500

    Haciendo cambios en el componente del formulario, se guardan los datos que ingresa el usuario, guarda el archivo del input de las fotos y se integra el mapa en la seccion del formulario para que el usuario ingrese su ubicación

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,40 +1,122 @@
 <template>
-    
-    <div class="form container mt-5 text-light">
- <form>
-     <label class="mb-5">Ingresa los datos de tu auto aquí</label>
-    <div class="form-group">
-       
-        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="No. de Placa">
-    </div>
-    <div class="form-group">
-       
-        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Marca">
-    </div>
-     <div class="form-group">
-      
-        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Color">
-    </div>
-    
-</form>
+  <div class="form container mt-5 text-light">
+    <form v-on:submit.prevent="serviceSave">
+      <label class="mb-5">Ingresa los datos de tu auto aquí</label>
+      <div class="form-group">
+        <input
+          v-model="newService.numPlaca"
+          type="text"
+          class="form-control"
+          id="formGroupExampleInput"
+          placeholder="No. de Placa"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          v-model="newService.marca"
+          type="text"
+          class="form-control"
+          id="formGroupExampleInput2"
+          placeholder="Marca"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          v-model="newService.color"
+          type="text"
+          class="form-control"
+          id="formGroupExampleInput2"
+          placeholder="Color"
+        />
+      </div>
+      <div class="form-group">
+        <input
+          @change="saveFoto"
+          v-if="uploadReady"
+          ref="myFiles"
+          type="file"
+          accept="image/*"
+          class="form-control"
+          id="formGroupExampleInput2"
+          placeholder="Imagen"
+        />
+      </div>
+      <label>Ingresa tu ubicación</label>
+      <GmapMap class="container mt-5"
+        v-bind:center =  "{lat : 19.4292730   , lng :  -99.1806473 } "
+        v-bind:zoom = " 18 "
+        style = " width: 80%; height: 300px "
+      >
+      <GmapMarker
+        v-bind:position=" {lat : 19.4292730  , lng : -99.1806473 } " 
+        v-bind:clickable="true"
+        v-bind:draggable="true"
         
-    </div>
+        />
+      </GmapMap>
+    
+    <button class="btn btn-outline-light btn-lg m-4" type="submit">Enviar</button>
+
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'form',
-  data () {
+  name: "form",
+  mounted(){
+      this.localizacion()
+  },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js PWA'
+      uploadReady:true,
+
+      newService: {
+        numPlaca: "",
+        marca: "",
+        color: "",
+        imagen: ""
+      }
+    };
+  },
+  methods: {
+    serviceSave() {
+      this.uploadReady=false
+      this.$nextTick( ()=>{
+        this.uploadReady=true;
+      })
+      console.log(this.newService);
+      this.newService={
+        numPlaca: "",
+        marca: "",
+        color: "",
+        imagen: ""
+      }
+      
+
+     
+    },
+    saveFoto(){
+     this.newService.imagen= this.$refs.myFiles.files
+     console.log(this.newService.imagen);
+      
+
+    },
+    localizacion(){
+     if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition( (position)=>{
+           console.log( position.coords.latitude, position.coords.longitude);
+       })
+      }
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
@@ -49,8 +131,6 @@ li {
 }
 
 a {
-  color: #35495E;
+  color: #35495e;
 }
-
-
 </style>
